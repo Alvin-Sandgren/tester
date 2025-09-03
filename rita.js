d0cc1d660784c28b1b09021a4ef49c1d427a1685
Alvin <alvin.sandgren@elev.ga.ntig.se>
2025-09-03 08:44:29 +0200

    ain förkortar

--- rita.js
+++ rita.js
@@ -3,12 +3,7 @@ const ctx = canvas.getContext('2d');
 
 let scrollX = 0;
 
-let ball = {
-  x: 400,
-  y: 300,
-  r: 20,
-  speed: 5
-};
+let ball = { x: 400, y: 300, r: 40, speed: 5 };
 
 const houses = [
   {x:200,y:250,w:120,h:100,c:'#FF8C00'},
@@ -19,101 +14,72 @@ const houses = [
 ];
 
 let keys = {};
+document.addEventListener('keydown', e => keys[e.key] = true);
+document.addEventListener('keyup', e => keys[e.key] = false);
 
-document.addEventListener('keydown', e => {
-  keys[e.key] = true;
-});
-
-document.addEventListener('keyup', e => {
-  keys[e.key] = false;
-});
+function circle(x, y, r, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI*2);
+  ctx.fill();
+}
 
 function drawMan(mx, my) {
-  ctx.fillStyle='#ffe0bd';
-  ctx.beginPath();
-  ctx.arc(mx,my-50,12,0,Math.PI*2);
-  ctx.fill();
-
+  circle(mx, my-50, 12, '#ffe0bd');
   ctx.fillStyle='orange';
   ctx.fillRect(mx-8,my-40,16,35);
-
   ctx.fillStyle='black';
   ctx.fillRect(mx-8,my-5,6,20);
   ctx.fillRect(mx+2,my-5,6,18);
 }
 
 function draw() {
-  ctx.fillStyle='#87CEEB';
-  ctx.fillRect(0,0,canvas.width,canvas.height);
+  ctx.fillStyle='#87CEEB'; ctx.fillRect(0,0,canvas.width,canvas.height);
+  ctx.fillStyle='#3CB371'; ctx.fillRect(0,canvas.height-150,canvas.width,200);
+  ctx.fillStyle='#555'; ctx.fillRect(0,canvas.height-60,canvas.width,60);
 
-  ctx.fillStyle='#3CB371';
-  ctx.fillRect(0,canvas.height-150,canvas.width,200);
+  ctx.fillStyle="white";
+  for (let i=0;i<canvas.width;i+=40) ctx.fillRect(i, canvas.height-30,20,5);
+  ctx.fillStyle="#4E4F52";
+  for (let i=0;i<canvas.width;i+=120) ctx.fillRect(i, canvas.height-30,20,5);
 
-  ctx.fillStyle='#555';
-  ctx.fillRect(0,canvas.height-60,canvas.width,60);
-
-  ctx.fillStyle = "rgba(255, 255, 255, 1)";
-  for (let i = 0; i < canvas.width; i += 40) {
-      ctx.fillRect(i, canvas.height - 30, 20, 5);
-  }
-  ctx.fillStyle = "rgba(78, 79, 82, 1)";
-  for (let i = 0; i < canvas.width; i += 120) {
-      ctx.fillRect(i, canvas.height - 30, 20, 5);
-  }
-
-  ctx.fillStyle = 'yellow';
-  ctx.beginPath();
-  ctx.arc(ball.x, ball.y, ball.r, 0, Math.PI*2);
-  ctx.fill();
+  circle(ball.x, ball.y, ball.r, "yellow");
 
   houses.forEach(h=>{
     let x=h.x-scrollX, y=canvas.height-h.y;
-
-    ctx.fillStyle=h.c; 
-    ctx.fillRect(x,y,h.w,h.h);
-
-    ctx.fillStyle='#333';
-    ctx.beginPath();
-    ctx.moveTo(x,y);
-    ctx.lineTo(x+h.w/2,y-40);
-    ctx.lineTo(x+h.w,y);
-    ctx.fill();
-
-    ctx.fillStyle='#654321';
-    ctx.fillRect(x+h.w/2-15,y+h.h-40,30,40);
-
-    ctx.fillStyle='#B0E0E6';
-    ctx.fillRect(x+10,y+20,30,25);
-    ctx.strokeStyle='#000';
-    ctx.strokeRect(x+10,y+20,30,25);
+    ctx.fillStyle=h.c; ctx.fillRect(x,y,h.w,h.h);
+    ctx.fillStyle='#333'; ctx.beginPath();
+    ctx.moveTo(x,y); ctx.lineTo(x+h.w/2,y-40); ctx.lineTo(x+h.w,y); ctx.fill();
+    ctx.fillStyle='#654321'; ctx.fillRect(x+h.w/2-15,y+h.h-40,30,40);
+    ctx.fillStyle='#B0E0E6'; ctx.fillRect(x+10,y+20,30,25);
+    ctx.strokeStyle='#000'; ctx.strokeRect(x+10,y+20,30,25);
   });
 
   let lx=houses[1].x+houses[1].w+40-scrollX, ly=canvas.height-60;
-  ctx.fillStyle='#444';
-  ctx.fillRect(lx,ly-100,8,100);
-  ctx.fillStyle='#FFD700';
-  ctx.beginPath();
-  ctx.arc(lx+4,ly-110,15,0,7);
-  ctx.fill();
+  ctx.fillStyle='#444'; ctx.fillRect(lx,ly-100,8,100);
+  circle(lx+4, ly-110, 15, '#FFD700');
 
   let mx1=houses[3].x+houses[3].w/2-scrollX; 
   let my1=canvas.height-60;
   drawMan(mx1,my1);
-
 }
 
-function update() {
-  if (keys['d']) scrollX += 5;
-  if (keys['a']) scrollX = Math.max(0, scrollX-5);
-
+function moveBall() {
+  if (keys['d']) scrollX += 10;
+  if (keys['a']) scrollX = Math.max(0, scrollX-10);
   if (keys['w']) ball.y -= ball.speed;
   if (keys['s']) ball.y += ball.speed;
 }
 
-function gameloop() {
-  update();
-  draw();
-  requestAnimationFrame(gameloop);
+function clampBall() {
+  ball.x = Math.max(ball.r, Math.min(canvas.width - ball.r, ball.x));
+  ball.y = Math.max(ball.r, Math.min(canvas.height - ball.r, ball.y));
 }
 
-gameloop();
+function loop() {
+  moveBall();
+  clampBall();
+  draw();
+  requestAnimationFrame(loop);
+}
+loop();
